262278b11695474c26951eab691b12abbf615d08
the-gobc-github <chmpdvwork@gmail.com>
2017-08-04 22:09:28 +0200

    calendar init

---
+++ site/pages/calendar.php
@@ -0,0 +1,25 @@
+<table class="calendar" align="center" border=1 cellpadding=2>
+
+<tr>
+
+<th>Sun <th>Mon <th>Tue <th>Wed <th>Thu <th>Fri <th>Sat
+
+</tr>
+
+<?php
+
+for ($i = 1; $i <= 5;$i++) {
+	echo "<tr>";
+	for ($j = 1; $j <= 7; $j++) {
+		echo "<td>";
+		$x = ((($i - 1) * 7) + $j);
+		if ($x <= 31) {
+			echo $x;
+		}
+		echo "</td>";
+	}
+	echo "</tr>";
+}
+
+echo date('l jS \of F Y h:i:s A');
+?>
--- site/pages/templates/default.php
+++ site/pages/templates/default.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title></title>
-    <link href="../public/css/style.css.php" rel="stylesheet"/>
+    <link href="../public/css/style.css" rel="stylesheet"/>
   </head>
   <body>
         <div class="menubar">
